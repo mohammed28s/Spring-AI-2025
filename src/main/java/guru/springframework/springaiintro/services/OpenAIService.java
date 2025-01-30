3aa50fd179b7d8c3ae9ea2fb07c40dc9ae5e477d
Mohammed Al Salhi <mohd_28s@Mohammeds-Mac-mini.local>
2025-01-30 16:56:14 +0400

    The Spring AIU API has been added

---
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
@@ -0,0 +1,13 @@
+package guru.springframework.springaiintro.services;
+
+/**
+ * Created by mohammed, Spring Framework Guru.
+ */
+
+
+public interface OpenAIService {
+
+
+    String getAnswer(String question);
+
+}
