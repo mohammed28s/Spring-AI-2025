3c200e164938bccdaead340fd2fc7071ad6c52c0
Mohammed Al Salhi <mohd_28s@Mohammeds-Mac-mini.local>
2025-01-30 15:37:14 +0400

    The spring boot project has been updated

--- src/main/java/guru/springframework/springaiintro/SpringAiIntroApplication.java
+++ src/main/java/guru/springframework/springaiintro/SpringAiIntroApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAiIntroApplication {
 
+	// This is Spring AI project
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiIntroApplication.class, args);
 	}
