566f7c777bf771e1231ab5c7e525880353fe4634
Mohammed Al Salhi <mohd_28s@Mohammeds-Mac-mini.local>
2025-02-02 21:58:34 +0400

    The the sevice has been tested with juint

--- src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -19,16 +19,16 @@ public class OpenAIServiceImpl implements OpenAIService{
     private final ChatModel chatModel;
 
     public OpenAIServiceImpl(ChatModel chatModel) {
-        this.chatModel = chatModel;
+        this.chatModel = chatModel;  //This is chatModel used with all types of LLM
     }
 
     @Override
     public String getAnswer(String question) {
         PromptTemplate promptTemplate = new PromptTemplate(question);
-        Prompt prompt = promptTemplate.create();
+        Prompt prompt = promptTemplate.create();  //This is to create new question
 
-        ChatResponse response = chatModel.call(prompt);
+        ChatResponse response = chatModel.call(prompt);  //This is to answer thr question
 
-        return response.getResult().getOutput().getContent();
+        return response.getResult().getOutput().getContent();  //These three functions for getting the answer
     }
 }
--- src/test/java/guru/springframework/springaiintro/services/OpenAIServiceImplTest.java
+++ src/test/java/guru/springframework/springaiintro/services/OpenAIServiceImplTest.java
@@ -4,19 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class OpenAIServiceImplTest {
 
     @Autowired
-    OpenAIService openAIService;
+    OpenAIServiceImpl openAIService;
 
     @Test
-    void getAnswer() {
+    void getAnswer(){
 
         String answer = openAIService.getAnswer("Tell me a dad joke.");
-        System.out.println("Got the answer");
+        System.out.println("Got the answer"+"\n");
         System.out.println(answer);
+
     }
+
+
+
+
+
+
 }
